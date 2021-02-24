da9702078edced7802b2f2c93e19bde8fa8fd496
Ma Tester <lulamae1983@gmail.com>
2021-02-24 18:20:24 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function turnOn() {
+
+  document.querySelector("img").setAttribute("src", "images/lighton.png");
+
+}
+
+
+function turnOff() {
+
+  document.querySelector("img").setAttribute("src", "images/lightoff.png");
+}
